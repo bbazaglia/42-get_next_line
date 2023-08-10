264547344218c8059f654628eadcf0521c56ddbb
Beatriz <bbazagli@student.42sp.org.br>
2023-08-10 10:43:48 -0300

    files required by the subject

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbazagli <bbazagli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/09 11:22:34 by bbazagli          #+#    #+#             */
+/*   Updated: 2023/08/09 11:25:07 by bbazagli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char *get_next_line(int fd);
+
+#endif
