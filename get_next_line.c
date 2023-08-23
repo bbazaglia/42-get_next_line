baff73d6ea4592865600c6dc94ad387abab91059
Beatriz <bbazagli@student.42sp.org.br>
2023-08-23 16:01:56 -0300

    tutorial in production

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bbazagli <bbazagli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 11:24:42 by bbazagli          #+#    #+#             */
-/*   Updated: 2023/08/10 17:19:30 by bbazagli         ###   ########.fr       */
+/*   Updated: 2023/08/23 16:01:05 by bbazagli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,119 +18,101 @@
 > What is a file descriptor?
 * A file descriptor is a non-negative integer that uniquely identifies an open file in a computer's operating system.
 
--> How can we read from a file descriptor?
-* We can use the read() function to read the data previously written to a file.
-* The read() function returns the number of bytes actually read, or -1 if an error occurred.
-
--> What is a buffer?
-* A buffer is a temporary storage area that stores data while it is being transferred from one place to another.
-* In this program, the buffer is used to temporarily hold data read from the file descriptor until complete lines can be extracted.
-
 -> Static vs. Non-Static Variables?
 * The memory for static variables is allocated once when the program starts and is retained until the program terminates.
 * The memory for non-static variables is allocated every time the function is called and is released when the function ends.
 
--> The usage of a static buffer in this project:
-* The static buffer will be allocated once and persist until the program ends.
-* As the function reads lines from the file descriptor and stores them in the buffer, it can continue reading from where it left off in the previous iteration.
+-> The usage of a static buff in this project:
+* The static buff will be allocated once and persist until the program ends.
+* As the function reads lines from the file descriptor and stores them in the buff, it can continue reading from where it left off in the previous iteration.
 
--> Note: attention to concurrency issues.
-* If multiple threads or parts of the program access the same static buffer simultaneously, it can lead to unexpected behavior or data corruption.
-
--> Why the buffer size impacts the performance of the program?
-* The buffer size is the number of bytes that the read() function will read from the file descriptor at each iteration.
-- A very small buffer size (e.g., 1) means the function reads only one byte at a time from the file descriptor.
+-> Why the buff size impacts the performance of the program?
+* The buff size is the number of bytes that the read() function will read from the file descriptor at each iteration.
+- A very small buff size (e.g., 1) means the function reads only one byte at a time from the file descriptor.
     This could lead to many I/O operations, making the process less efficient due to the overhead associated with each read operation.
     Additionally, processing data byte by byte can introduce complexity when dealing with multi-byte character encodings.
-- A very large buffer size can be efficient in terms of reading large amounts of data in one go.
-    However, it might lead to excessive memory usage, particularly if lines are shorter than the buffer size. Unused memory in the buffer might go to waste.
-- A relatively large buffer size allows the function to read a substantial amount of data at once from the file descriptor.
+- A very large buff size can be efficient in terms of reading large amounts of data in one go.
+    However, it might lead to excessive memory usage, particularly if lines are shorter than the buff size. Unused memory in the buff might go to waste.
+- A relatively large buff size allows the function to read a substantial amount of data at once from the file descriptor.
     This can be efficient because reading larger chunks of data minimizes the number of I/O operations, reducing overhead.
-    At the same time, the buffer is not so large that it wastes memory when lines are shorter than the buffer size.
+    At the same time, the buff is not so large that it wastes memory when lines are shorter than the buff size.
     
--> What buffer size should we use?
-* The choice of BUFFER_SIZE depends on a trade-off between efficiency and memory usage. 
-* A reasonable buffer size is typically in the range of a few kilobytes (e.g., 1024 or 4096 bytes).
-
--> How to handle different buffer and line sizes?
-* The function has to dynamically allocate memory for the buffer and line as needed.
-* Instead of using a fixed-size buffer, you can allocate memory for the buffer based on the BUFFER_SIZE value.
-* The buffer size can be changed by recompiling the program with a different value for BUFFER_SIZE.
-* Similarly, when constructing the line, you can dynamically allocate memory to accommodate the variable line ft_strlens.
-
--> How to handle lines longer than the buffer size?
-1) Keep Track of Incomplete Line Fragments.
-    If a line spans across multiple buffer reads, you need to keep track of the incomplete line fragments at the end of the buffer.
-2) Memory Reallocation.
-    When extending a line that spans across buffers, use memory reallocation to ensure that you have enough space to accommodate the entire line. 
-3) Reset Buffer and Update Buffer Position and Size.
-    After processing an incomplete line that spans multiple buffers, you need to reset the buffer and update the buffer position and size to reflect the remaining data in the buffer. 
-4) Concatenate Line Fragments from Different Buffers.
-    If a line spans multiple buffers, concatenate the data from different buffers to reconstruct the complete line.
-    You can use the ft_strjoin() function to concatenate the line fragments from the previous buffer with the line fragments from the current buffer.
-5) Null-Termination.
-    Ensure that the reconstructed line is null-terminated so that string manipulation functions can be safely used on it.
-
--> How to handle the situation in which the content of the file changes between successive calls to get_next_line()?
-* If  the file pointer is moved or data is added/removed,
-	it can lead to undefined behavior or unexpected results.
-    If new data is added to the file after a read() operation but before a subsequent read(),
-	the function might miss or incorrectly process the new data. 
-    Similarly, if data is removed,
-	the function might read data that no longer exists.
-* After every read operation,
-	reset the buffer and the file position to the beginning of the buffer. 
-    This ensures that the function starts reading from a consistent position in the file.
+-> What buff size should we use?
+* The choice of buff_SIZE depends on a trade-off between efficiency and memory usage. 
+* A reasonable buff size is typically in the range of a few kilobytes (e.g., 1024 or 4096 bytes).
 
 -> Return values of get_next_line():
-* correct behavior: the line that was read (including the terminating ’\n’ character,
-		if present);
+* correct behavior: the line that was read (including the terminating ’\n’ character, if present);
 * there is nothing to read or an error occurred: NULL. */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
-{
-	static char	buffer[BUFFER_SIZE];
-	char		*line;
-	int			bytes_read;
-	int			buffer_position;
-	char		current_char;
-	char		*new_line;
+/* Let's say that what is written in our file is: C a r o l e \n  B i a
+                                                  1 2 3 4 5 6  7  8 9 10
+                                                  
+Suppose our buffer size is 8. 
 
-	bytes_read = 1;
-	buffer_position = 0;
-    line = NULL;
+When read is called for the first time, these are the bytes we get: "Carole\nB"
+
+However, the line breaks with the occurrence of the newline. 
+
+So, what do we truly want to print? "Carole\n"
+
+What about the "B"? We shall save it somewhere, because when read is called again, the bytes read will be "ia" and "B" will be lost.
+
+Okay, now let's move to the tricky part: the name of the function is "get NEXT line". 
+In the first call of the read function, line would be NULL, meanwile the "next line" would be "Carole\n".
+
+But what if I want to get what's next (aka the second next line)?
+We take the fragment of the line that was read in the call before plus the bytes we've just read in the current call of read : "Bia".
+
+So, after all, what the heck is this "line" variable? 
+If you're trying to get your first next line, it will be initialized as NULL and then updated to the bytes read.
+If you're trying to get any other next line, it will be initialized as the remainder of the last read call made and then updated to the sum of it with the bytes read. */
+
+char    *read_and_join(int fd, char *line)
+{
+    char *buffer;
+    int bytes_read;
     
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
+    // we're reading for the fist time or the previous call didn't left behind any fragment after the newline
+    if (!line)
+        line = ft_calloc(1, 1);
+
+    // allocate memory for the buffer && check if the allocation was successful 
+    buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+    if (!buffer)
+        return (NULL);
         
-    while (buffer[buffer_position] != '\n' && buffer[buffer_position] != '\0')
-	{
-		// if the buffer is empty, read more data from the file descriptor
-		if (buffer_position == bytes_read)
-		{
-			bytes_read = read(fd, buffer, BUFFER_SIZE);
-			buffer_position = 0;
-		}
-		if (bytes_read <= 0)
-			return (NULL);
-		current_char = buffer[bytes_read];
-        // if newline character encountered, return the current line
-        if (current_char == '\n')
+    // itialize the "bytes read" to 1 so it executes the loop at least once
+    bytes_read = 1;
+
+    // keep reading as long as we don't get to the end of the line and there are still bytes to read
+    while (!ft_strchr(buffer, '\n') && bytes_read != 0)
+    {
+        bytes_read = read(fd, buffer, BUFFER_SIZE);
+        // if the read operation fails, return NULL
+        if (bytes_read == -1)
         {
-	        new_line = ft_calloc(ft_strlen(line) + buffer_position + 1, sizeof(char));
-	        if (new_line == NULL)
-		        return (NULL);
-	        // copy content of line into new_line
-	        ft_strlcpy(new_line, line, ft_strlen(line) + 1);
-        	// copy content of buffer from 0 to buffer_position into new_line starting at line ft_strlen
-	        ft_strlcpy(new_line, buffer, buffer_position + 1);
-	        return (new_line);
+            free(buffer);
+            return (NULL);
         }
-        // append current character to the line
-        line = ft_strjoin(line, current_char);
-        // move to the next character in the buffer
-	    buffer_position++;
-	}
+        // null terminate the buffer so we can work with this data as a string (every string is null terminated)
+        buffer[bytes_read] = '\0';
+        line = ft_strjoin(line, buffer);
+    }
+    return (free(buffer), (line));
 }
+
+// char	*get_next_line(int fd)
+// {
+//     char        *next_line;
+//     static char *buff;
+    
+// 	if (fd < 0 || BUFFER_SIZE <= 0)
+// 		return (NULL);       
+
+//     buff = read_and_join(fd, buff);
+//     if (!buff)
+//         return (NULL);
+// }
